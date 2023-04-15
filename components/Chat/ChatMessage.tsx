ff11eb616577628ceae918794b82c7e3cd34a2f5
garyokeeffe <garyjokeeffe@gmail.com>
2023-04-14 17:24:11 -0700

    add light/dark mode messages

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -3,16 +3,24 @@ import { FC } from "react";
 
 interface Props {
   message: Message;
+  lightMode: "light" | "dark";
 }
 
-export const ChatMessage: FC<Props> = ({ message }) => {
+export const ChatMessage: FC<Props> = ({ message, lightMode }) => {
+  const isAssistant = message.role === "assistant";
+  const bgColor = isAssistant
+    ? lightMode === "light" ? "bg-neutral-100" : "dark:bg-[#434654]"
+    : lightMode === "light" ? "dark:bg-[#343541]" : "";
+  const textColor = isAssistant ? "text-neutral-900" : "dark:text-white";
+  const borderColor = isAssistant && lightMode === "light" ? "border border-neutral-300" : "dark:border-none";
+
   return (
     <div
-      className={`flex justify-center px-[120px] py-[30px] whitespace-pre-wrap] ${message.role === "assistant" ? "dark:bg-[#434654] dark:text-neutral-100 bg-neutral-100 text-neutral-900 border border-neutral-300 dark:border-none" : "dark:bg-[#343541] dark:text-white text-neutral-900"}`}
+      className={`flex justify-center px-[120px] py-[30px] whitespace-pre-wrap] ${bgColor} ${textColor} ${borderColor}`}
       style={{ overflowWrap: "anywhere" }}
     >
       <div className="w-[650px] flex">
-        <div className="mr-4 font-bold min-w-[40px]">{message.role === "assistant" ? "AI:" : "You:"}</div>
+        <div className="mr-4 font-bold min-w-[40px]">{isAssistant ? "ChatGPT:" : "You:"}</div>
 
         <div className="whitespace-pre-wrap">{message.content}</div>
       </div>
