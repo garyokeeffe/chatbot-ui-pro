46ce578cab2a496d5320d82dce62b23472d03ef6
garyokeeffe <garyjokeeffe@gmail.com>
2023-04-14 17:07:54 -0700

    adding css changes

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 
-export const Navbar: FC = () => {
+interface Props {
+  lightMode: "light" | "dark";
+}
+
+export const Navbar: FC<Props> = ({ lightMode }) => {
+  const themeClass = lightMode === "light" ? "orange-theme" : "black-theme";
   return (
-    <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
+    <div className={`flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between ${themeClass}`}>
       <div className="font-bold text-3xl flex items-center">
         <a
           className="ml-2 hover:opacity-50"
--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -11,7 +11,7 @@ export const SidebarButton: FC<Props> = ({ text, lightMode, icon, onClick }) => 
 	const themeClass = lightMode === "light" ? "orange-theme" : "black-theme";
   return (
     <div
-      className="flex hover:bg-[#343541] py-2 px-4 rounded-md cursor-pointer w-[200px] items-center"
+      className={`flex py-2 px-4 rounded-md cursor-pointer w-[200px] items-center ${themeClass}`}
       onClick={onClick}
     >
       <div className="mr-3">{icon}</div>
