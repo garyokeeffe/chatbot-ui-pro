8662989688c1638299cd76d68b0156b217d13886
garyokeeffe <garyjokeeffe@gmail.com>
2023-04-14 17:59:31 -0700

    fix closing bracket

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -11,12 +11,12 @@ export const ChatMessage: FC<Props> = ({ message, lightMode }) => {
   const bgColor = isAssistant
     ? lightMode === "light" ? "bg-neutral-100" : "dark:bg-[#f9f9fa]"
     : lightMode === "light" ? "dark:bg-black" : "";
-  const textColor = isAssistant ? "text-neutral-900" : "dark:text-[#f9f9fa]";
+  const textColor = isAssistant ? "text-neutral-900" : "dark:text-neutral-100";
   const borderColor = isAssistant && lightMode === "light" ? "border border-neutral-300" : "dark:border-none";
 
   return (
     <div
-      className={`flex justify-center px-[120px] py-[30px] whitespace-pre-wrap] ${bgColor} ${textColor} ${borderColor}`}
+      className={`flex justify-center px-[120px] py-[30px] whitespace-pre-wrap ${bgColor} ${textColor} ${borderColor}`}
       style={{ overflowWrap: "anywhere" }}
     >
       <div className="w-[650px] flex">
