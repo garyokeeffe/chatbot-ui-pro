f5624aaf699866e634249370bbc2794e788b3ed5
garyokeeffe <garyjokeeffe@gmail.com>
2023-04-20 22:03:57 -0700

    chat message overhaul

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -3,48 +3,64 @@ import { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 
-
 interface Props {
   message: Message;
   lightMode: "light" | "dark";
   conversation: Conversation; // Add conversation prop
-  index: number; 
+  index: number;
+  onSelect: (updatedConversation: Conversation) => void;
 }
 
-  // Update the conversation state and localStorage
-  onSelect(updatedConversation);
-
-  try {
-    const response = await fetch('/api/report', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        conversationId: updatedConversation.id,
-        messageIndex,
-        feedback,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-  } catch (error) {
-    console.error('Failed to send feedback:', error);
-  }
-};
-
-export const ChatMessage: FC<Props> = ({ message, lightMode }) => {
-  const handleFeedback = async (messageIndex: number, feedback: 'good' | 'bad') => {
+export const ChatMessage: FC<Props> = ({
+  message,
+  lightMode,
+  conversation,
+  index,
+  onSelect,
+}) => {
+  const handleFeedback = async (messageIndex: number, feedback: "good" | "bad") => {
     const updatedConversation = { ...conversation };
     updatedConversation.messages[messageIndex].feedback = feedback;
+
+    // Update the conversation state and localStorage
+    onSelect(updatedConversation);
+
+    try {
+      const response = await fetch("/api/report", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          conversationId: updatedConversation.id,
+          messageIndex,
+          feedback,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.error("Failed to send feedback:", error);
+    }
+  };
+
   const isAssistant = message.role === "assistant";
   const bgColor = isAssistant
-    ? lightMode === "light" ? "bg-neutral-100" : "bg-black"
-    : lightMode === "light" ? "bg-white" : "bg-black";
+    ? lightMode === "light"
+      ? "bg-neutral-100"
+      : "bg-black"
+    : lightMode === "light"
+    ? "bg-white"
+    : "bg-black";
   const textColor = isAssistant
-    ? lightMode === "light" ? "text-orange-500" : "text-orange-400"
-    : lightMode === "light" ? "text-neutral-900" : "text-white";
-  const borderColor = isAssistant && lightMode === "light" ? "border border-neutral-300" : "dark:border-none";
+    ? lightMode === "light"
+      ? "text-orange-500"
+      : "text-orange-400"
+    : lightMode === "light"
+    ? "text-neutral-900"
+    : "text-white";
+  const borderColor =
+    isAssistant && lightMode === "light" ? "border border-neutral-300" : "dark:border-none";
 
   return (
     <div
@@ -55,20 +71,20 @@ export const ChatMessage: FC<Props> = ({ message, lightMode }) => {
         <div className="mr-4 font-bold min-w-[40px]">{isAssistant ? "AI:" : "You:"}</div>
 
         <div className="whitespace-pre-wrap">{message.content}</div>
-        {message.role === 'assistant' && (
-        <div className="flex items-center">
-          <FontAwesomeIcon
-            icon={faThumbsUp}
-            className="mx-1 text-green-500 cursor-pointer"
-            onClick={() => handleFeedback(index, 'good')}
-          />
-          <FontAwesomeIcon
-            icon={faThumbsDown}
-            className="mx-1 text-red-500 cursor-pointer"
-            onClick={() => handleFeedback(index, 'bad')}
-          />
-        </div>
-      )}
+        {message.role === "assistant" && (
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              icon={faThumbsUp}
+              className="mx-1 text-green-500 cursor-pointer"
+              onClick={() => handleFeedback(index, "good")}
+            />
+            <FontAwesomeIcon
+              icon={faThumbsDown}
+              className="mx-1 text-red-500 cursor-pointer"
+              onClick={() => handleFeedback(index, "bad")}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
