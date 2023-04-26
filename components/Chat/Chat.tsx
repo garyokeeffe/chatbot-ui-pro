ddce50f9eb911fe76aa884230241e1fbfe46f775
garyokeeffe <garyjokeeffe@gmail.com>
2023-04-25 22:39:47 -0700

    make text overflow scroll

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -29,43 +29,44 @@ export const Chat: FC<Props> = ({ model, messages, conversation, lightMode, load
 
   return (
     <div className="flex flex-col w-full h-full p-4">
-      {messages?.length === 0 ? (
-        <>
-          <div className="flex justify-center pt-8">
-            <ModelSelect
-              model={model}
-              onModelSelect={onModelSelect}
+  {messages?.length === 0 ? (
+    <>
+      <div className="flex justify-center pt-8">
+        <ModelSelect
+          model={model}
+          onModelSelect={onModelSelect}
+        />
+      </div>
+
+      <div className="flex-1 text-4xl text-center text-neutral-300 pt-[100px]">satGPT</div>
+    </>
+  ) : (
+    <>
+      <div className="flex-1 overflow-y-auto flex-grow">
+        <div className="text-center py-3 dark:bg-black dark:text-white text-neutral-500 text-sm border border-b-neutral-300 dark:border-none">AI: {OpenAIModelNames[model]}</div>
+
+        {messages?.map((message, index) => (
+          <div key={index}>
+            <ChatMessage
+              message={message}
+              lightMode={lightMode}
+              onSelect={onSelect}
+              conversation={conversation}
+              index={index}
             />
           </div>
+        ))}
 
-          <div className="flex-1 text-4xl text-center text-neutral-300 pt-[100px]">satGPT</div>
-        </>
-      ) : (
-        <>
-          <div className="flex-1 overflow-auto flex-grow">
-            <div className="text-center py-3 dark:bg-black dark:text-white text-neutral-500 text-sm border border-b-neutral-300 dark:border-none">AI: {OpenAIModelNames[model]}</div>
-
-            {messages?.map((message, index) => (
-            <div key={index}>
-                <ChatMessage
-                  message={message}
-                  lightMode={lightMode}
-                  onSelect={onSelect}
-                  conversation={conversation}
-                  index={index}
-                />
-              </div>
-            ))}
-
-            {loading && <ChatLoader />}
-            <div ref={messagesEndRef} />
-          </div>
-        </>
-      )}
-
-      <div className="flex-shrink-0 mx-auto w-full max-w-[800px]">
-        <ChatInput onSend={onSend} />
+        {loading && <ChatLoader />}
+        <div ref={messagesEndRef} />
       </div>
-    </div>
+    </>
+  )}
+
+  <div className="flex-shrink-0 mx-auto w-full max-w-[800px]">
+    <ChatInput onSend={onSend} />
+  </div>
+</div>
+
   );
 };
